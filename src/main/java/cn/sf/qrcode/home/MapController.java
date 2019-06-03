db93d9a7c685ede8b9876bd1889490fb973bb701
sunfen <sunfen@git>
2019-06-03 17:08:32 +0800

    update

--- src/main/java/cn/sf/qrcode/home/MapController.java
+++ src/main/java/cn/sf/qrcode/home/MapController.java
@@ -5,6 +5,7 @@ package cn.sf.qrcode.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -20,10 +21,10 @@ public class MapController {
 
     
 
-    @GetMapping("view")
-    public String view( Model model) throws Exception {
-    	
-    
+    @GetMapping("view/{lat}/{lng}")
+    public String view( Model model, @PathVariable Double lat, @PathVariable Double lng ) throws Exception {
+    	model.addAttribute("lat", lat);
+    	model.addAttribute("lng", lng);
     	return "map";
     }
     
